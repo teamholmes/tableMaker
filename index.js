97f362a1f871201af09aca820e40a4525ca4dd38
Russell Holmes <russellholmes@Russells-MacBook-Pro.local>
2015-07-27 22:12:41 +0100

    a whole heap of test code

--- index.js
+++ index.js
@@ -1,5 +1,110 @@
 /// https://quickleft.com/blog/creating-and-publishing-a-node-js-module/
 
+
+var Kitten = function(){
+
+}
+
+Kitten.prototype.setName = function(name) {
+	this.name = name;
+	return this;
+}
+
+Kitten.prototype.uppercaseName = function() {
+	this.name = this.name.toUpperCase();
+	return this;
+}
+
+Kitten.prototype.getName = function() {
+	return this.name;
+}
+
+var charlie = new Kitten();
+console.log('The cats name is : ' + charlie.setName('david').uppercaseName().getName());
+
+
+var reversename = function(name) {
+	return name.split('').reverse().join('');
+};
+
+var mynamespace = {
+
+	getName : function() {
+		return this.name
+	},
+	setName : function(name) {
+		this.name = name;
+	},
+	obtainName : function (callback) {
+		return callback(this.name);
+	}
+};
+
+var df = mynamespace;
+
+df.setName('alex');
+console.log(df.getName());
+console.log(df.obtainName(reversename));
+
+
+
+
+
+
+var mortekai = {
+	test: function(mget) {
+		console.log('+++++++')
+		var mm = { 
+			get: mget.bind(mm)
+        };
+ 
+        return mm;
+    }
+};
+
+var vvv = function dddd() {
+	console.log( this.x);
+};
+
+console.log(mortekai);
+console.log(mortekai.test(vvv));
+console.log(mortekai.test(vvv));
+
+
+
+var example = function() {
+	console.log('a');
+      return {
+        add: function() {
+        	console.log('c');
+        },
+         subtract: function() {
+        	console.log('d');
+        }
+      };
+  };
+
+example().add();
+example().subtract();
+
+
+
+var obj =  {
+        add: function() {
+        	console.log('CC');
+        },
+         subtract: function() {
+        	console.log('DD');
+        }
+
+      };
+
+obj.add();
+obj.subtract();
+      //console.log(obj.subtract());
+
+
+
 var tableModule = require('./table');
 
 var x = 5;
--- test/test.table.js
+++ test/test.table.js
@@ -5,7 +5,7 @@ var tableModule = require('../table');
 describe('Table tests', function() {
   describe('Table creation', function () {
     it('it should create a new table object with my chosen dom id', function () {
-    	var domId = 'testId';
+		var domId = 'testId';
 		var newTable = new tableModule(domId);
 		var newTable1 = new tableModule(domId + '1');
       assert.equal(newTable.getTableId(), domId);
